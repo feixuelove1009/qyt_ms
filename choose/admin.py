f9d30c666aa3422994b36b11bbc76ac2591a648a
feixuelove1009 <feixuelym@sina.com>
2017-08-05 12:38:14 +0800

    修改admin界面

--- choose/admin.py
+++ choose/admin.py
@@ -2,18 +2,27 @@ from django.contrib import admin
 from choose import models
 # Register your models here.
 
+
 class TestBedsAdmin(admin.ModelAdmin):
-    list_display = ("bed_name", "rock")
+    list_display = ("rock","bed_name")
+
+    class meta:
+        verbose_name = "试验台"
+        verbose_name_plural = "试验台"
 
 
 class RowsAdmin(admin.ModelAdmin):
-    # list_display = ("time_zone", "test_bed")
+    list_display = ("test_bed", "time_zone")
     pass
+
+
+class NodesAdmin(admin.ModelAdmin):
+    list_display = ("weekday", "row", "owner")
 
 
 admin.site.register(models.Rocks)
 admin.site.register(models.Rows, RowsAdmin)
 admin.site.register(models.User)
-admin.site.register(models.Nodes)
+admin.site.register(models.Nodes, NodesAdmin)
 admin.site.register(models.Weekdays)
 admin.site.register(models.TestBeds, TestBedsAdmin)
--- choose/models.py
+++ choose/models.py
@@ -5,9 +5,9 @@ from django.db import models
 
 class User(models.Model):
     # 英文名
-    e_name = models.CharField(max_length=128)
+    e_name = models.CharField(max_length=128, verbose_name="英文名")
     # 中文名
-    c_name = models.CharField(max_length=128)
+    c_name = models.CharField(max_length=128, verbose_name="中文名")
 
     def __str__(self):
         return self.c_name
@@ -15,9 +15,9 @@ class User(models.Model):
 
 class Nodes(models.Model):
 
-    owner = models.ForeignKey("User", blank=True, null=True)
-    weekday = models.ForeignKey("Weekdays")
-    row = models.ForeignKey("Rows")
+    owner = models.ForeignKey("User", blank=True, null=True, verbose_name="拥有者")
+    weekday = models.ForeignKey("Weekdays", verbose_name="星期")
+    row = models.ForeignKey("Rows", verbose_name="时间段")
 
     def __str__(self):
         return self.row.test_bed.rock.rock_name + ":  " + \
@@ -26,31 +26,31 @@ class Nodes(models.Model):
 
 
 class Rows(models.Model):
-    time_zone = models.CharField(max_length=32)
-    test_bed = models.ForeignKey("TestBeds")
+    time_zone = models.CharField(verbose_name="时间段", max_length=32)
+    test_bed = models.ForeignKey("TestBeds", verbose_name="试验台")
 
     def __str__(self):
         return self.test_bed.rock.rock_name + ":  " + self.test_bed.bed_name + ":  " + self.time_zone
 
 
 class Weekdays(models.Model):
-    day_name = models.CharField(max_length=32)
+    day_name = models.CharField(max_length=32, verbose_name="星期")
 
     def __str__(self):
         return self.day_name
 
 
 class TestBeds(models.Model):
-    bed_name = models.CharField(max_length=32)
-    rock = models.ForeignKey("Rocks")
+    bed_name = models.CharField(max_length=32, verbose_name="试验台")
+    rock = models.ForeignKey("Rocks", verbose_name="种类")
 
     def __str__(self):
-        return self.bed_name
+        return self.rock.rock_name + ":" + self.bed_name
 
 
 class Rocks(models.Model):
 
-    rock_name = models.CharField(max_length=64)
+    rock_name = models.CharField(max_length=64, verbose_name="种类")
 
     def __str__(self):
         return self.rock_name
