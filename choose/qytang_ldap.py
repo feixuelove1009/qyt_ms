078b4a86b99705a6ed4817aadfa1b3c8edd24e35
feixuelove1009 <feixuelym@sina.com>
2017-08-09 10:03:17 +0800

    little change

--- choose/qytang_ldap.py
+++ choose/qytang_ldap.py
@@ -9,11 +9,11 @@ server = Server('ldap://172.16.66.6')
 def qytldap(username, password):
     try:
         c = Connection(server, auto_bind=AUTO_BIND_NO_TLS, read_only=True,check_names=True, user="qytang\\"+username, password=password)
-        c.search(search_base = 'dc=qytang,dc=com', search_filter = '(&(samAccountName=' + username + '))',attributes = ['memberOf','Sn'],paged_size = 5)
+        c.search(search_base='dc=qytang,dc=com', search_filter='(&(samAccountName=' + username + '))',attributes = ['memberOf','Sn'],paged_size = 5)
         return (c.response[0]['attributes']['memberOf'],c.response[0]['attributes']['Sn'])
     except Exception:
         return None
 
 
 if __name__ == "__main__":
-    print(qytldap('rsec-zhumin','Cisc0123'))
+    pass
